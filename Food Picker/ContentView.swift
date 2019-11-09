cdaf32ba6d6e99e289ebcd5cf7bc0c8e38585fdc
Irving M Santaolaya <irvingcodes@gmail.com>
2019-11-08 17:36:52 -0800

    Initial Commit

---
+++ Food Picker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food Picker
+//
+//  Created by Irving Martinez on 11/8/19.
+//  Copyright © 2019 Irving Martinez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
